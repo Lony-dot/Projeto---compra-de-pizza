eea71510e0b4eb4ca853366431c73dfb98687e3b
Lony <loonydarck@hotmail.com>
2022-07-26 08:00:55 -0300

    adicionando as formas das pizzas

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log(pizzaJson);
+pizzaJson.map((item, index) =>{
+    let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true); 
+    // Preencher as informações em pizzaitem
+    
+    document.querySelector('.pizza-area').append( pizzaItem ); //append adiciona um novo valor dentro de uma chave existente
+});
