a11e8874a34ec4cebf3e6a5d4b98e997f4887a85
Lony <loonydarck@hotmail.com>
2022-07-27 08:44:18 -0300

    Adicionando o Modal ao  site

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const cs = (el)=>document.querySelectorAll(el);
 pizzaJson.map((item, index) =>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true); 
     // Preencher as informações em pizzaitem
+
+    pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
         pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
